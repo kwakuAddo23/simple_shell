855e9754824be00821bc918db2ff5d84dcd1f17e
root <kppoku@gmail.com>
2023-04-26 13:23:36 -0700

    exists.c file

---
+++ exists.c
@@ -0,0 +1,60 @@
+#include "shell.h"
+
+/**
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the copied string
+ */
+char *_strncpy(char *dest, const char *src, size_t n)
+{
+	size_t i;
+	char *s = dest;
+
+	for (i = 0; i < n - 1 && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return s;
+}
+
+/**
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
+ */
+char *_strncat(char *dest, const char *src, size_t n)
+{
+	size_t i, j;
+	char *s = dest;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; j < n && src[j] != '\0'; j++)
+		dest[i + j] = src[j];
+	dest[i + j] = '\0';
+
+	return s;
+}
+
+/**
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: a pointer to the memory area s where the character is found or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return s;
+		s++;
+	}
+
+	return NULL;
+}
